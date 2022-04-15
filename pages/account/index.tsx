4cca4bf0f202103fc7769c730c9e8d592f0ff35c
Anurag <kr.anurag24@gmail.com>
2022-04-15 13:53:13 +0000

    auth done

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -1,15 +1,32 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 
 import { Box, useDisclosure } from '@chakra-ui/react'
 import NavigationBar from '../../components/Navigation/NavigationBar.component'
 import { SignUpModal } from '../../components'
 
+import { auth } from '../../utils/firebaseUtils'
+import { onAuthStateChanged } from 'firebase/auth'
+
 const Account: NextPage = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const [user, setUser] = useState<any>()
+
+    console.log(user)
+
+    onAuthStateChanged(auth, (user) => {
+        user ? setUser(user) : setUser(null)
+    })
 
     return (
         <>
-            <SignUpModal isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
+            {!user && (
+                <SignUpModal
+                    isOpen={isOpen}
+                    onOpen={onOpen}
+                    onClose={onClose}
+                />
+            )}
 
             <Box>
                 <NavigationBar />
