7bfa52c85651489dea36d5af1a7089efc1f76af1
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2022-08-03 00:15:25 +0530

    write logs

--- layouts/MyEvents/Ticket.layout.tsx
+++ layouts/MyEvents/Ticket.layout.tsx
@@ -66,7 +66,7 @@ export default function TicketLayout({
                             'in',
                             `("${ethers.utils.getAddress(contractAddress)}")`
                         )
-
+                    console.log(data)
                     setQr(data?.[0]?.uuid)
                 } catch (error) {}
             }
