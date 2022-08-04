71694f3613ece129ddd3396d7ba69af10f212f84
anoushk1234 <anoushka1828@gmail.com>
2022-08-04 10:49:50 +0530

    try getserverside

--- pages/event/[address].tsx
+++ pages/event/[address].tsx
@@ -11,71 +11,71 @@ import { useEffect, useState } from 'react'
 import NavigationBar from '../../components/Navigation/NavigationBar.component'
 import EventLayout from '../../layouts/Event/Event.layout'
 import { Skeleton } from '@chakra-ui/react'
-import { useRouter } from 'next/router'
 import axios from 'axios'
 import { gqlEndpoint } from '../../utils/subgraphApi'
-import Head from 'next/head'
+import { ethers } from 'ethers'
 
-const Event: NextPage = () => {
-    const router = useRouter()
-    const { address } = router.query
+const Event: NextPage = ({ event }: any) => {
+    const [featEvent, setFeatEvent] = useState<Event>(event)
 
-    const [featEvent, setFeatEvent] = useState<Event>({
-        id: '',
-        title: '',
-        childAddress: '',
-        category: {
-            event_type: '',
-            category: [''],
-        },
-        image: {
-            image: '',
-            gallery: [],
-            video: '',
-        },
-        eventHost: '',
-        fee: 0,
-        date: '',
-        description: {
-            short_desc: '',
-            long_desc: '',
-        },
-        seats: 0,
-        owner: '',
-        // price: 0,
-        type: '',
-        tickets_available: 0,
-        tickets_sold: 0,
-        buyers: [],
-        isSolana: false,
-        isHuddle: false,
-    })
+    return (
+        <Box minH="100vh" h="full" overflow="hidden" bg="blackAlpha.50">
+            <NavigationBar mode="white" />
+            <Box p="4" />
+            <Flex
+                justify="center"
+                mx="auto"
+                mt="16"
+                px="6"
+                w="full"
+                maxW="1400px"
+                experimental_spaceX="10"
+            >
+                <Box maxW="1000px" w="full">
+                    {featEvent ? (
+                        <Skeleton isLoaded={featEvent.id !== ''}>
+                            <EventLayout event={featEvent} />
+                        </Skeleton>
+                    ) : (
+                        <Flex alignItems={'center'}>Event Doesn't Exist</Flex>
+                    )}
+                </Box>
+            </Flex>
+        </Box>
+    )
+}
 
+export default Event
+
+export async function getServerSideProps({ query }: any) {
+    const address = query.address
+    let parsedEvent
     async function getFeaturedEvents() {
         const featuredQuery = {
             operationName: 'fetchFeaturedEvents',
             query: `query fetchFeaturedEvents {
-          childCreatedEntities(where:{id:"${String(address).toLowerCase()}"}) {
-            id
-            title
-            childAddress
-            category
-            ticketsBought{
-                id
-            }
-            link
-            description
-            date
-            fee
-            image
-            eventHost
-            seats
-            buyers{
-              id
-            }
-          }
-          
-    }`,
+                childCreatedEntities(where:{id:"${String(
+                    address
+                ).toLowerCase()}"}) {
+                    id
+                    title
+                    childAddress
+                    category
+                    ticketsBought{
+                        id
+                    }
+                    link
+                    description
+                    date
+                    fee
+                    image
+                    eventHost
+                    seats
+                    buyers{
+                        id
+                    }
+                }
+            }`,
         }
         try {
             const res = await axios({
@@ -98,96 +98,79 @@ const Event: NextPage = () => {
     function UnicodeDecodeB64(str: any) {
         return decodeURIComponent(atob(str))
     }
-    const parseFeaturedEvents = (event: any): Event => {
-        let type: string = JSON.parse(
-            UnicodeDecodeB64(event.category)
-        ).event_type
-        let category: CategoryType = JSON.parse(
-            UnicodeDecodeB64(event.category)
-        )
-        let image: ImageType = JSON.parse(UnicodeDecodeB64(event.image))
-        let desc: DescriptionType = JSON.parse(
-            UnicodeDecodeB64(event.description)
-        )
-        return {
-            id: event.id,
-            title: event.title,
-            childAddress: event.childAddress,
-            category: category,
-            image: image,
-            eventHost: event.eventHost,
-            fee: Number(event.fee) / 10 ** 18,
-            date: event.date,
-            description: desc,
-            seats: event.seats,
-            owner: event.eventHost,
-            link: event.link,
-            type: type,
-            tickets_available: event.seats - event.ticketsBought?.length,
-            tickets_sold: event.ticketsBought?.length,
-            buyers: event.buyers,
-            slides: image.gallery,
-            isSolana: false,
-            isHuddle: event.link.includes('huddle'),
-        } as Event
-    }
-
-    const getSolanaEvents = async () => {
-        if (address) {
-            const { data } = await axios.post('/api/getSolanaEvents', {
-                address: address,
-            })
-            setFeatEvent(data)
+    const parseFeaturedEvents = (event: any) => {
+        if (event) {
+            let type: string = JSON.parse(
+                UnicodeDecodeB64(event.category)
+            ).event_type
+            let category: CategoryType = JSON.parse(
+                UnicodeDecodeB64(event.category)
+            )
+            let image: ImageType = JSON.parse(UnicodeDecodeB64(event.image))
+            let desc: DescriptionType = JSON.parse(
+                UnicodeDecodeB64(event.description)
+            )
+            return {
+                id: event.id,
+                title: event.title,
+                childAddress: event.childAddress,
+                category: category,
+                image: image,
+                eventHost: event.eventHost,
+                fee: Number(event.fee) / 10 ** 18,
+                date: event.date,
+                description: desc,
+                seats: event.seats,
+                owner: event.eventHost,
+                link: event.link,
+                type: type,
+                tickets_available: event.seats - event.ticketsBought?.length,
+                tickets_sold: event.ticketsBought?.length,
+                buyers: event.buyers,
+                slides: image.gallery,
+                isSolana: false,
+                isHuddle: event.link.includes('huddle'),
+            } as Event
+        } else {
+            return null
         }
     }
-    useEffect(() => {
-        getFeaturedEvents()
-            .then((res) => {
-                // console.log(res.data.childCreatedEntities[0],"res")
-                if (res.data.childCreatedEntities[0]) {
-                    const data: Event = parseFeaturedEvents(
-                        res.data.childCreatedEntities[0]
-                    )
+    const getSolanaEvents = async () => {
+        const event = await axios.get(
+            `${process.env.NEXT_PUBLIC_MONGO_API}/getEvent/${address}`
+        )
+        if (event.data) {
+            const data = event.data
+            return {
+                ...data,
+                owner: data.eventHost,
+                childAddress: address as string,
+                category: JSON.parse(data.category),
+                image: JSON.parse(data.image),
+                description: JSON.parse(data.description),
+                isSolana: true,
+            }
+        } else {
+            return null
+        }
+    }
 
-                    // console.log(data, 'data')
-                    setFeatEvent(data)
-                } else {
-                    getSolanaEvents()
-                }
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    }, [address])
+    if (address) {
+        const isEtherAddress = ethers.utils.isAddress(address)
 
-    return (
-        <Box minH="100vh" h="full" overflow="hidden" bg="blackAlpha.50">
-            <Head>
-                <meta property="og:title" content={featEvent.title} />
-                <meta
-                    property="og:image"
-                    content={`https://mp-og.vercel.app/${featEvent.title}`}
-                />
-            </Head>
-            <NavigationBar mode="white" />
-            <Box p="4" />
-            <Flex
-                justify="center"
-                mx="auto"
-                mt="16"
-                px="6"
-                w="full"
-                maxW="1400px"
-                experimental_spaceX="10"
-            >
-                <Box maxW="1000px" w="full">
-                    <Skeleton isLoaded={featEvent.id !== ''}>
-                        <EventLayout event={featEvent} />
-                    </Skeleton>
-                </Box>
-            </Flex>
-        </Box>
-    )
+        if (isEtherAddress) {
+            const event = await getFeaturedEvents()
+            parsedEvent = parseFeaturedEvents(
+                event.data.childCreatedEntities[0]
+            )
+        } else {
+            parsedEvent = await getSolanaEvents()
+        }
+    }
+
+    return {
+        props: {
+            event: parsedEvent,
+        },
+    }
 }
-
-export default Event
